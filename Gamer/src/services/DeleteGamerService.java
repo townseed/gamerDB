684e14e12332b84756669a67bc00ce1720d191e7
Alexander Soiefer <69055950+Alexander-Soiefer@users.noreply.github.com>
2024-02-15 20:32:35 -0500

    delete bug fix 1

--- Gamer/src/services/DeleteGamerService.java
+++ Gamer/src/services/DeleteGamerService.java
@@ -33,13 +33,13 @@ public class DeleteGamerService {
 			}
 			else {
 				System.out.println("from gamePieceService: Error in SQL execution");
+				return false;
 			}
 		} catch (SQLException e) {
 			
 			e.printStackTrace();
 			return false;
 		}
-		return true;
 	}
 
 }
